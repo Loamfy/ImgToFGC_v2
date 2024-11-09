b8dc7ffcf416e69399b332a06b4868644f365788
Loamf <kabirovalmaz20102019@gmail.com>
2024-11-09 12:05:12 +0500

    fix for GitHub Actions

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,4 +1,4 @@
 @echo off
 echo "Warning! Code will use \"python -m pip\" to install requirements
 pause
-python -m pip install webcolors nuitka pillow click rich
+python -m pip install -r requirements.txt
